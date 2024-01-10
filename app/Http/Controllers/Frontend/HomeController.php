240d51fe1006c32491f8ec41242a4feea37bc831
nguyenanbinh <nguyenanbinh.98.dn@gmail.com>
2024-01-10 15:29:45 +0700

    Show products in frontend

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -3,15 +3,40 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 use App\Models\Slider;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function index() : View {
+    function index(): View
+    {
         $sliders = Slider::where('status', 1)->get();
-        
-        return view('frontend.home.index', compact('sliders'));
+
+        $categories = Category::where(['show_at_home' => 1, 'status' => 1])->get();
+
+        return view(
+            'frontend.home.index',
+            compact(
+                'sliders',
+                'categories'
+            )
+        );
+    }
+
+    function showProduct(string $slug): View
+    {
+        $product = Product::with(['productImages', 'productSizes', 'productOptions'])
+            ->where(['slug' => $slug, 'status' => 1])
+            ->firstOrFail();
+        $relatedProducts = Product::where('category_id', $product->category_id)
+            ->where('id', '!=', $product->id)
+            ->take(8)
+            ->latest()
+            ->get();
+
+        return view('frontend.pages.product-view', compact('product', 'relatedProducts'));
     }
 }
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
     use HasFactory;
+
+    function category() : BelongsTo {
+        return $this->belongsTo(Category::class);
+    }
+
+    function productImages() : HasMany {
+        return $this->hasMany(ProductGallery::class);
+    }
+
+    function productSizes() : HasMany {
+        return $this->hasMany(ProductSize::class);
+    }
+    
+    function productOptions() : HasMany {
+        return $this->hasMany(ProductOption::class);
+    }
 }
